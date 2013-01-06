2469201dc85a48f3d07b04ec5d9bfb5d0d270893
gsnewmark <gildraug@gmail.com>
2013-01-06 15:28:27 +0200

    Initial draft of imageboard posts resource.

--- src/clj_chan/handler.clj
+++ src/clj_chan/handler.clj
@@ -6,24 +6,51 @@
             [cheshire.core :as j]))
 
 
-(def postbox-counter (atom 0))
+;; ## ~Model~
 
-(l/defresource postbox
-  :method-allowed? (l/request-method-in :post)
-  :post! (swap! postbox-counter inc)
-  :handle-created "Your submission was accepted."
-  :available-media-types ["application/json"])
+(defrecord Post [id author date content])
 
-(l/defresource postbox-list
+(def posts "In-memory storage of posts." (atom #{}))
+
+(defn add-post [post]
+  "Add a post to a DB. Argument is a map with optional keys :author and
+:content."
+  (let [{:keys [author content] :or {author "Anon" content "sth"}} post]
+    (swap! posts conj (->Post 0 author (java.util.Date.) content))))
+
+;; ## Resources
+
+;; TODO add html representation
+;; List of all posts.
+(l/defresource posts-list
   :method-allowed? (l/request-method-in :get)
-  :handle-ok (fn [ctx]
-               (j/generate-string
-                {:posts @postbox-counter} {:escape-non-ascii true}))
-  :available-media-types ["application/json"])
+  :available-media-types ["application/json"]
+  :handle-ok (fn [ctx] (j/generate-string @posts {:escape-non-ascii true})))
+
+;; curl request to test:
+;; curl -v -H "Accept: application/json" -H "Content-type: application/json" -X POST -d ' {"post":{"author":"o","content":"c"}}'  http://localhost:3000/imageboard/posts/create
+;; Creation of a new post. Expects an AJAX request with body
+;; {"post":{["author":name],["content":content]}}, where "author" and
+;; "content" elements are optional.
+(l/defresource create-post
+  :method-allowed? (l/request-method-in :post)
+  :available-media-types ["application/json"]
+  :handle-created "Submission accepted"
+  :post! (fn [ctx]
+           (-> (:request ctx)
+               :body
+               clojure.java.io/reader
+               (j/parse-stream true)
+               :post
+               add-post)))
+
+;; ## Routes
 
 (c/defroutes chan-routes
-  (c/POST "/post" [] postbox)
-  (c/GET "/post" [] postbox-list))
+  (c/ANY "/imageboard/posts" [] posts-list)
+  (c/ANY "/imageboard/posts/create" [] create-post))
+
+;; ## App's ring handler
 
 (def app
   (h/site chan-routes))
