39660f0d92e554ad888c8592bd4b51af6bf34cd3
gsnewmark <gildraug@gmail.com>
2013-01-18 00:23:03 +0200

    Clean deps.

--- project.clj
+++ project.clj
@@ -5,9 +5,6 @@
                  :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure     "1.5.0-RC2"]
                  [compojure               "1.1.3"]
-                 [liberator               "0.8.0"]
-                 [cheshire                "5.0.1"]
-                 [enlive                  "1.0.1"]
                  [org.webbitserver/webbit "0.4.6"]
                  [enfocus                 "1.0.0-beta2"]]
   :main         clj-chan.handler
--- src/clj/clj_chan/handler.clj
+++ src/clj/clj_chan/handler.clj
@@ -2,11 +2,7 @@
   "Entry point of an app - Ring handler."
   (:require [compojure.core :as c]
             [compojure.handler :as handler]
-            [compojure.route :as r]
-            [liberator.core :as l]
-            [liberator.representation :as lr]
-            [cheshire.core :as j]
-            [net.cgrand.enlive-html :as h])
+            [compojure.route :as r])
   (:import [org.webbitserver WebServer WebServers WebSocketHandler])
   (:gen-class :main true))
 
@@ -15,7 +11,7 @@
 
 (defrecord Post [id author date content])
 
-(def posts "In-memory storage of posts." (atom #{}))
+(def posts "In-memory storage of posts." (atom []))
 
 (defn add-post [post posts]
   "Add a post to a DB. Argument is a map with optional keys :author and
@@ -26,7 +22,7 @@
     (swap! posts conj post)
     (dissoc post :id)))
 
-;; Web socket
+;; ## Web socket
 
 (def csrv (WebServers/createWebServer 8008))
 (def connections (atom #{}))
@@ -43,7 +39,6 @@
 (.add csrv "/chatsocket"
       (proxy [WebSocketHandler] []
         (onOpen    [c]   (do
-                           (println @posts)
                            (swap! connections conj c)
                            (.send c (pr-str (map (partial into {}) @posts)))))
         (onClose   [c]   (swap! connections disj c))
@@ -52,58 +47,9 @@
 (defn -main [& m]
   (.start csrv))
 
-;; ## Views
-
-;; Takes a list of Post instances as an argument.
-(h/deftemplate index "public/index.html"
-  [posts]
-  [:header]              (h/do->
-                          (h/content "topic1")))
-
-;; ## Resources
-
-;; List of all posts.
-(l/defresource posts-list
-  :method-allowed?       (l/request-method-in :get)
-  :available-media-types ["application/json" "text/html"]
-  :handle-ok             (fn [ctx]
-                           (case (get-in ctx [:representation :media-type])
-                             "text/html" (apply str (index @posts))
-                             (j/generate-string @posts
-                                                {:escape-non-ascii true}))))
-
-;; Access to particular post.
-(l/defresource post
-  :method-allowed?       (l/request-method-in :get)
-  :available-media-types ["application/json"]
-  :handle-ok             (fn [ctx]
-                           (let [id (get-in ctx [:request :params :id])]
-                             (j/generate-string
-                              (first (filter #(= id (:id %)) @posts))
-                              {:escape-non-ascii true}))))
-
-;; curl request to test:
-;; curl -v -H "Accept: application/json" -H "Content-type: application/json" -X POST -d ' {"post":{"author":"o","content":"c"}}'  http://localhost:3000/imageboard/posts/create
-;; Creation of a new post. Expects an AJAX request with body
-;; {"post":{["author":name],["content":content]}}, where "author" and
-;; "content" elements are optional.
-(l/defresource create-post
-  :method-allowed?       (l/request-method-in :post)
-  :available-media-types ["application/json"]
-  :handle-created        "Submission accepted"
-  :post!                 (fn [ctx]
-                           (-> (get-in ctx [:request :body])
-                               clojure.java.io/reader
-                               (j/parse-stream true)
-                               :post
-                               add-post)))
-
 ;; ## Routes
 
 (c/defroutes chan-routes
-  (c/ANY       "/imageboard/posts"        [] posts-list)
-  (c/ANY       "/imageboard/posts/create" [] create-post)
-  (c/ANY       "/imageboard/posts/:id"    [] post)
   (r/resources "/")
   (r/not-found "Page not found"))
 
